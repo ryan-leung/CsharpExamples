f2ff94694a1cb5f3824d638b7bd423b22a4a9160
Ryan Leung <yanyan.ryan.leung@gmail.com>
2023-11-15 00:48:07 +0000

    update new examples

---
+++ CsharpMultithreading/CsharpMultithreading/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+/* This program demonstrates basic multithreading in C#, creating and running multiple 
+ * threads concurrently. Each thread simulates work by sleeping for a different number 
+ * of seconds. The Thread.Sleep method is used to introduce delays, and the console output
+ * reflects the activity of each thread.
+ */
+
+namespace CsharpMultithreading
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Get the main thread
+            Thread mainThread = Thread.CurrentThread;
+
+            // Print information about the main thread
+            Console.WriteLine("Main Thread.");
+
+            // Create and start five additional threads
+            for (int i = 1; i <= 5; i++)
+            {
+                int threadNumber = i;
+
+                // Create a new thread using a lambda expression
+                Thread newThread = new Thread(() =>
+                {
+                    // Print a message indicating that the thread is running
+                    Console.WriteLine($"Thread {threadNumber} is running.");
+
+                    // Call the Sleep method with the thread number
+                    Sleep(threadNumber);
+                });
+
+                // Start the new thread
+                newThread.Start();
+            }
+
+            // Wait for user input before exiting the program
+            Console.ReadLine();
+        }
+
+        // Method that simulates work by sleeping for a specified number of seconds
+        public static void Sleep(int seconds)
+        {
+            // Sleep for the specified number of seconds
+            Thread.Sleep(seconds * 1000);
+
+            // Print a message indicating the number of seconds slept
+            Console.WriteLine($"Slept {seconds} seconds");
+        }
+    }
+}
---
+++ SimpleWinFormsApp/SimpleWinFormsApp/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SimpleWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            monthCalendar1 = new MonthCalendar();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(29, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(470, 18);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(monthCalendar1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private MonthCalendar monthCalendar1;
+    }
+}
